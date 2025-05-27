5a575a7f27660b74b2b039a22f39f4a601b6c59d
TendTo <e.casablanca2@ncl.ac.uk>
2025-05-27 14:32:51 +0100

    feat: support ros bridge for airsim

--- newcastle_racing_ai/launch/nra_launch.py
+++ newcastle_racing_ai/launch/nra_launch.py
@@ -2,7 +2,7 @@ import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
 from ament_index_python import get_package_share_directory
 from launch.substitutions import LaunchConfiguration, TextSubstitution
 
@@ -31,6 +31,11 @@ def generate_launch_description():
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
                     os.path.join(get_package_share_directory("eufs_launcher"), "eufs_launcher.launch.py")
+                )
+            ),
+            IncludeLaunchDescription(
+                FrontendLaunchDescriptionSource(
+                    os.path.join(get_package_share_directory("rosbridge_server"), "launch", "rosbridge_websocket_launch.xml")
                 )
             ),
             Node(
